95a42f41b9b283a79ef904c8297190c869339b30
Kun Saw Thu Ra <149572796+KSThuRa@users.noreply.github.com>
2026-08-26 10:55:09 +0630

    Delete app directory

--- app/Repositories/Category/CategoryRepositoryInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Repositories\Category;
-
-interface CategoryRepositoryInterface {
-    public function index();
-    public function store($data);
-    public function show($id);
-}
